e3b1bf459ad1205f2b3ab5e0669bb5bbc94e9e33
JordyWiraArta <98179507+JordyWiraArta@users.noreply.github.com>
2022-12-29 17:56:41 +0700

    kedua

--- BeePark/app/src/main/java/edu/bluejack22_1/beepark/controllers/BuildingController.kt
+++ BeePark/app/src/main/java/edu/bluejack22_1/beepark/controllers/BuildingController.kt
@@ -1,6 +1,9 @@
 package edu.bluejack22_1.beepark.controllers
 
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
 class BuildingController {
-    private var db = Firebase
+    private var db = Firebase.firestore
 
 }
