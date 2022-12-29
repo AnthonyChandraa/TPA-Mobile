fb679f088830d690c6c97de5ab4b47f74ed8d6ff
AnthonyChandraa <anthonychd.22@gmail.com>
2022-12-29 20:34:54 +0700

    Login & Register using Email & Password

--- BeePark/app/src/main/java/edu/bluejack22_1/beepark/LoginActivity.kt
+++ BeePark/app/src/main/java/edu/bluejack22_1/beepark/LoginActivity.kt
@@ -1,27 +1,60 @@
 package edu.bluejack22_1.beepark
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var firebaseAuth: FirebaseAuth;
+    private lateinit var etEmail : EditText;
+    private lateinit var etPassword : EditText;
+    private lateinit var btnLogin : Button;
+    private lateinit var btnToRegis : Button;
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        firebaseAuth = Firebase.auth;
 
+//        val currentUser = firebaseAuth.currentUser
+//        if(currentUser != null){
+//            startActivity(Intent(this, HomeActivity::class.java))
+//        }
 
-        val btnToRegis = findViewById<Button>(R.id.btnRegis)
+        etEmail = findViewById(R.id.emailInput)
+        etPassword = findViewById(R.id.passwordInput)
+        btnLogin = findViewById(R.id.btnLogin)
+        btnToRegis = findViewById(R.id.btnRegis)
+
         btnToRegis.setOnClickListener{
             finishAndRemoveTask()
             startActivity(Intent(this, RegisActivity::class.java))
         }
 
-        // submit login
-        val btnLogin = findViewById<Button>(R.id.btnLogin)
         btnLogin.setOnClickListener{
-            finishAndRemoveTask()
-            startActivity(Intent(this, HomeActivity::class.java))
+            val email = etEmail.text.toString().trim()
+            val password = etPassword.text.toString().trim()
+
+            if(email.isNotEmpty() && password.isNotEmpty()){
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this){ task->
+                    if(task.isSuccessful){
+                        startActivity(Intent(this, HomeActivity::class.java))
+                    }else{
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "Login failed.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
         }
     }
 }
